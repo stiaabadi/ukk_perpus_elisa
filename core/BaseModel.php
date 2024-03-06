e85c2a7c1627cea6f65639e19541b43a579b40bb
stiaabadi <159861379+stiaabadi@users.noreply.github.com>
2024-03-05 18:30:12 -0800

    Update BaseModel.php

--- core/BaseModel.php
+++ core/BaseModel.php
@@ -7,7 +7,7 @@ class BaseModel
 
 	public function __construct()
 	{
-		$this->mysqli = new mysqli('localhost', 'root', '', 'ukk_perpus_aulia'); # sesuaikan dengan nama database masing - masing
+		$this->mysqli = new mysqli('localhost', 'root', '', 'ukk_perpus_elisa'); # sesuaikan dengan nama database masing - masing
 	}
 
 	public function getByUsername($username)
